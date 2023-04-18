84d3065ce70d45493f39949dafd4000f68cf1cf5
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-04-18 19:04:53 +0300

    Update AuthPage.jsx

--- src/components/AuthPage.jsx
+++ src/components/AuthPage.jsx
@@ -5,7 +5,9 @@ const AuthPage = props => {
     e.preventDefault();
     const { value } = e.target[0];
     axios
-      .post('http://localhost:3001/authenticate', { username: value })
+      .post('https://really-cool-chat-backend.vercel.app/authenticate', {
+        username: value,
+      })
       .then(r => props.onAuth({ ...r.data, secret: value }))
       .catch(e => console.log('error', e));
   };
